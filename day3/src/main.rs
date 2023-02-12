af3ecaaac772e9cb66250ff8569b71e4589f88c7
Martin <martin.hogstedt@hotmail.com>
2023-02-12 20:19:54 +0100

    close to finish day2 part2 but borrowing is hard

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,24 +1,120 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::vec::Vec;
 
 
 const INPUT: &str = "input";
+const TEST: &str = "test";
+const TOTAL: i32 = 4095;
 
 
 fn main() {
 
+    let mut rate: Vec<i128> = vec![0; 13];
+    let mut values: Vec<&str> = Vec::new();
+
     // Parse the imput file
-    if let Ok(lines) = read_lines(INPUT) {
+    if let Ok(lines) = read_lines(TEST) {
         for line in lines {
             if let Ok(ip) = line {
 
                 if !ip.is_empty() {
+                    let code: String = ip.parse().unwrap();
 
+                    part1(&code, &mut rate);                    
+                    values.push(&code);
                 }
             }
         }
     }
+
+    for value in &values {
+        println!("{}", value);
+    }
+    println!("Part 1: {}", calc_gamma_eps(&rate) * (TOTAL-calc_gamma_eps(&rate)));
+    println!("Part 2: {}", get_life_rating(&values, 0, true));
+}
+
+pub fn get_life_rating(values: &Vec<&str>, index: usize, oxy: bool) ->i128 {
+    let mut i: usize = index;
+
+    let mut zero: Vec<&str> = Vec::new();
+    let mut one: Vec<&str> = Vec::new();
+
+    for value in values {
+        value.chars().nth(i);
+        if value.chars().nth(i).unwrap() == '0' {
+            zero.push(&value);
+        } else {
+            one.push(&value);
+        }
+    }
+
+    i += 1;
+
+    if oxy {
+        if one.len() == 1 {
+            return bi_to_dec(one[0]);
+        } else if one.len() == zero.len() {
+            return get_life_rating(&one, i, oxy);
+        } else {
+            return get_life_rating(&zero, i, oxy);
+        }
+    } else {
+        if zero.len() == 1 {
+            return bi_to_dec(zero[0]);
+        } else if one.len() == zero.len() {
+            return get_life_rating(&zero, i, oxy);
+        } else {
+            return get_life_rating(&one, i, oxy);
+        }
+    }
+
+
+}
+
+pub fn part1(code: &String, rate: &mut [i128]) {
+    for (i, bit) in code.chars().enumerate() {
+        rate[i] += bit as i128 - 48;
+    }
+    rate[12] += 1;
+}
+
+pub fn bi_to_dec(code: &str) -> i128 {
+    let len = code.chars().count() as u32;
+    let mut dec: i128 = 0;
+
+    for (i, value) in code.chars().enumerate() {
+
+
+        dec += (value as i128 - 48)*2_i128.pow((len - i as u32 - 1).try_into().unwrap());
+    }
+    return dec;
+
+}
+
+pub fn calc_gamma_eps(rate: &[i128]) -> i32 {
+
+    let threshold = rate[12]/2;
+    let mut pos: i32 = 0;
+    let mut gamma: i32 = 0;
+
+    for value in rate {
+        if value >= &threshold {
+
+            gamma += 2_i32.pow((11-pos).try_into().unwrap());
+        }
+
+
+        if pos == 11 {
+            break;
+        }
+        pos += 1;
+    }
+
+    return gamma;
+
 }
 
 
