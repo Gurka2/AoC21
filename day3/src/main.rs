df6c951adb758a7f7b6c66ab0884e2e9c5449829
martin <marho227@student.liu.se>
2023-02-13 11:46:17 +0100

    finished day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -12,10 +12,10 @@ const TOTAL: i32 = 4095;
 fn main() {
 
     let mut rate: Vec<i128> = vec![0; 13];
-    let mut values: Vec<&str> = Vec::new();
+    let mut values = Vec::new();
 
     // Parse the imput file
-    if let Ok(lines) = read_lines(TEST) {
+    if let Ok(lines) = read_lines(INPUT) {
         for line in lines {
             if let Ok(ip) = line {
 
@@ -23,55 +23,64 @@ fn main() {
                     let code: String = ip.parse().unwrap();
 
                     part1(&code, &mut rate);                    
-                    values.push(&code);
+                    values.push(code.to_string());
                 }
             }
         }
     }
 
-    for value in &values {
-        println!("{}", value);
-    }
     println!("Part 1: {}", calc_gamma_eps(&rate) * (TOTAL-calc_gamma_eps(&rate)));
-    println!("Part 2: {}", get_life_rating(&values, 0, true));
+    println!("Part 2: {}", get_life_rating(&values, 0, true) * get_life_rating(&values, 0, false));
+    println!("{}", bi_to_dec("0000000000000"));
 }
 
-pub fn get_life_rating(values: &Vec<&str>, index: usize, oxy: bool) ->i128 {
+pub fn get_life_rating(values: &Vec<String>, index: usize, oxy: bool) -> i128 {
+
+    if values.len() == 1 {
+        return bi_to_dec(&values[0]);
+    }
+
     let mut i: usize = index;
 
-    let mut zero: Vec<&str> = Vec::new();
-    let mut one: Vec<&str> = Vec::new();
+    let mut zero = Vec::new();
+    let mut one = Vec::new();
 
     for value in values {
         value.chars().nth(i);
         if value.chars().nth(i).unwrap() == '0' {
-            zero.push(&value);
+            zero.push(value.to_string());
         } else {
-            one.push(&value);
+            one.push(value.to_string());
         }
     }
 
     i += 1;
 
     if oxy {
-        if one.len() == 1 {
-            return bi_to_dec(one[0]);
+        if one.len() == 1 && 1 == zero.len() {
+            return bi_to_dec(&one[0]);
         } else if one.len() == zero.len() {
             return get_life_rating(&one, i, oxy);
         } else {
-            return get_life_rating(&zero, i, oxy);
+            if one.len() > zero.len() {
+                return get_life_rating(&one, i, oxy);
+            } else {
+                return get_life_rating(&zero, i, oxy);
+            }
         }
     } else {
-        if zero.len() == 1 {
-            return bi_to_dec(zero[0]);
+        if one.len() == 1 && 1 == zero.len() {
+            return bi_to_dec(&zero[0]);
         } else if one.len() == zero.len() {
             return get_life_rating(&zero, i, oxy);
         } else {
-            return get_life_rating(&one, i, oxy);
+            if one.len() > zero.len() {
+                return get_life_rating(&zero, i, oxy);
+            } else {
+                return get_life_rating(&one, i, oxy);
+            }
         }
     }
-
-
 }
 
 pub fn part1(code: &String, rate: &mut [i128]) {
@@ -88,7 +97,10 @@ pub fn bi_to_dec(code: &str) -> i128 {
     for (i, value) in code.chars().enumerate() {
 
 
-        dec += (value as i128 - 48)*2_i128.pow((len - i as u32 - 1).try_into().unwrap());
+        dec += (value as i128 - 48)*2_i128
+            .pow((len - i as u32 - 1)
+                 .try_into()
+                 .unwrap());
     }
     return dec;
 
